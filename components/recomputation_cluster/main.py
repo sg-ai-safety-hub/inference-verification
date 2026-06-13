11f9137155552b5d9323422d9d1ea2f4bdaa4133
Wayne Tee <waynetee37@gmail.com>
2026-06-14 00:33:52 +0800

    Update cors

--- components/recomputation_cluster/main.py
+++ components/recomputation_cluster/main.py
@@ -21,7 +21,7 @@ env = Settings()  # type: ignore
 app = FastAPI(dependencies=[Depends(require_key(env.api_key))])
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
